bca82387cdcee78c2292c7ee7b22c782a60f520a
MongieLee <le30n@outlook.com>
2020-12-08 07:20:07 +0800

    update

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -53,22 +53,42 @@ function onError(data: any) {
 
 const User: React.FC = () => {
   useEffect(() => {
-    var map = new AMap.Map("container");
-    // AMap.plugin("AMap.ToolBar", function () {
-    //   var toolbar = new AMap.Geolocation();
-    //   map.addControl(toolbar);
-    // });
+    const nanyou: number[] = [113.573518, 22.237448]; //南油大酒店经纬度
+    var lnglat = new AMap.LngLat(nanyou[0], nanyou[1]);
+    var map = new AMap.Map("container", {
+      viewMode: "2D", //设置地图模式
+      lang: "zh_cn", //设置地图语言类型
+      zoom: 13,
+      center: nanyou,
+    });
+    var m1 = new AMap.Marker({
+      position: nanyou,
+    });
+    map.add(m1);
+    // var pixel = map.lngLatToContainer(lnglat);
+    // console.log(pixel)
+
+    var circle = new AMap.Circle({
+      center: nanyou,
+      radius: 100, //签到范围半径
+      borderWeight: 1,
+      strokeOpacity: 1,
+      fillOpacity: 0.4,
+    });
+
+    circle.setMap(map);
     let geolocation;
     map.plugin("AMap.Geolocation", function () {
       geolocation = new AMap.Geolocation({
         enableHighAccuracy: true, //是否使用高精度定位，默认:true
-        timeout: 10000, //超过10秒后停止定位，默认：无穷大
+        timeout: 10000, //超过10秒后停止定位，默认：5s
+        buttonPosition: "RB", //定位按钮的停靠位置
         buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
-        zoomToAccuracy: true, //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-        buttonPosition: "RB",
+        // zoomToAccuracy: true,
       });
       map.addControl(geolocation);
       geolocation.getCurrentPosition();
+      map.setFitView();
       AMap.event.addListener(geolocation, "complete", onComplete); //返回定位信息
       AMap.event.addListener(geolocation, "error", onError); //返回定位出错信息
     });
@@ -200,7 +220,7 @@ const User: React.FC = () => {
               bottom: 0,
               left: "50%",
               zIndex: 2,
-              translateX: "-50%",
+              transform: "translateX(-50%)",
             }}
             color="primary"
           >
